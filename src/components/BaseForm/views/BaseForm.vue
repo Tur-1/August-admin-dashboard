574850c3538c5cd6f0fc67a08f5bd2cdf7749548
HP-TUR <tur.1@hotmail.com>
2023-09-03 14:53:12 +0300

    f....

--- src/components/BaseForm/views/BaseForm.vue
+++ src/components/BaseForm/views/BaseForm.vue
@@ -1,9 +1,7 @@
 <template>
   <form @submit.prevent="$emit('onSubmit')" enctype="multipart/form-data">
     <div class="card border-0 shadow p-2 pb-4 mb-4">
-      <div
-        class="card-header mx-lg-4 p-0 py-3 py-lg-3 mb-4 mb-md-0 d-flex justify-content-between"
-      >
+      <div class="card-header mx-lg-4 p-0 py-3 py-lg-3 mb-4 mb-md-0 d-flex justify-content-between">
         <h3 class="h5 mb-0">{{ title }}</h3>
 
         <SubmitButton :title="submitTitle" />
--- src/components/SubmitButton/index.vue
+++ src/components/SubmitButton/index.vue
@@ -1,20 +1,7 @@
 <template>
-  <button
-    class="btn btn-gray-800 d-flex"
-    :class="class"
-    type="submit"
-    :disabled="FormStore.onProgress ?? props.onProgress"
-  >
+  <button class="btn btn-gray-800" :class="class" type="submit" :disabled="FormStore.onProgress ?? props.onProgress">
     <span>{{ title }}</span>
-    <div
-      class="spinner-border spinner-border-sm opacity-0"
-      role="status"
-      :class="{
-        'opacity-100 onProgress': FormStore.onProgress ?? props.onProgress,
-      }"
-    >
-      <span class="visually-hidden">Loading...</span>
-    </div>
+
   </button>
 </template>
 
